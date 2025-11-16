71ef573e0015eddd6129d30531e095ed900ac31b
Robert Breutzmann <robert.breutzmann@gmail.com>
2025-11-16 10:10:44 -0600

    City Functions - Step 1

---
+++ module-7/city_functions.py
@@ -0,0 +1,22 @@
+# Breutzmann, Robert
+# CSD 325 - Advanced Python
+# Assignment 7.2
+# Due Date - 11/24/25
+
+def combineCity(city, country):
+    result = f"{city}, {country}"
+    return result
+
+city1 = "Madrid"
+country1 = "Spain"
+city2 = "Rome"
+country2 = "Italy"
+city3 = "Berlin"
+country3 ="Germany"
+combined1 = combineCity(city1, country1)
+combined2 = combineCity(city2, country2)
+combined3 = combineCity(city3, country3)
+
+print(combined1)
+print(combined2)
+print(combined3)
