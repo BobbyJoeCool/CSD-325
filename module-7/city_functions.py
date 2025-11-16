05942d130af2aa0a297f74c84c5db85ee871a166
Robert Breutzmann <robert.breutzmann@gmail.com>
2025-11-16 14:06:51 -0600

    Update Final Program

--- module-7/city_functions.py
+++ module-7/city_functions.py
@@ -15,12 +15,20 @@ city1 = "Madrid"
 country1 = "Spain"
 city2 = "Rome"
 country2 = "Italy"
+population2 = 2750000
 city3 = "Berlin"
 country3 ="Germany"
+population3 = 37000000
+language3 = "German"
+city4 = "Tripoli"
+country4 = "United States"
+language4 = "English"
 combined1 = combineCity(city1, country1)
-combined2 = combineCity(city2, country2)
-combined3 = combineCity(city3, country3)
+combined2 = combineCity(city2, country2, population2)
+combined3 = combineCity(city3, country3, population3, language3)
+combined4 = combineCity(city4, country4, language4)
 
 print(combined1)
 print(combined2)
 print(combined3)
+print(combined4)
